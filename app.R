26e87a9140c6b60ef5c93010191be301f818edc0
Luke Yang <lukey.ubc@qq.com>
2023-02-27 21:17:10 -0800

    add app

--- app.R
+++ app.R
@@ -8,19 +8,135 @@
 #
 
 library(shiny)
+library(bslib)
+library(leaflet)
+library(DT)
+library(ggplot2)
+library(plotly)
+library(thematic)
+library(forcats)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  sliderInput(inputId='slider',
-              label='I am a label',
-              min=0,
-              max=10,
-              value=5 ) )
-  
-# Define server logic required to draw a histogram
+data <- read.csv("data/processed_communities.csv") |>
+  select(c('state_name','community','latitude','longitude','violent_crime_rate','type','population'))
+
+# Reload when saving the app
+options(shiny.autoreload = TRUE)
+
+ui <- navbarPage('Crime Rate Finder App',
+                 theme = bs_theme(bootswatch = 'lux'),
+                 tabPanel('Data Exploration',
+                          sidebarLayout(column(3,selectInput(inputId = 'state',
+                                               label = 'Select State:',
+                                               choices = unique(data$state_name),
+                                               selected = 'California'),
+                                   selectInput(inputId = 'type',
+                                               label = 'Select Community Type:',
+                                               choices = unique(data$type),
+                                               selected = 'city')),
+                          column(9, leaflet::leafletOutput(outputId = 'map')))
+                          
+                 ),
+                 tabPanel('Correlation',
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput(inputId = 'state_plot',
+                                          label = 'Select State:',
+                                          choices = unique(data$state_name),
+                                          selected = 'California')
+                            ),
+                            mainPanel(
+                              tabsetPanel(
+                                tabPanel('Population Correlation',
+                                         plotlyOutput(outputId = 'lineplot')
+                                ),
+                                tabPanel('Communities in the State',
+                                         DT::DTOutput(outputId = 'table'))
+                              )
+                            )
+                          ))
+                 
+)
+
 server <- function(input, output, session) {
+  
+  ## LINEPLOT
+  
+  
+  filtered_data <-reactive({
 
-    }
+    data |>
+      dplyr::filter(state_name == input$state) |>
+      dplyr::filter(type == input$type)
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+  })
+  
+  filtered_data_plot <-reactive({
+    
+    data |>
+      dplyr::filter(state_name == input$state_plot)
+    
+  })
+  
+  output$lineplot <- plotly::renderPlotly({ 
+    
+    thematic::thematic_shiny()
+    
+    plotly::ggplotly(
+      filtered_data_plot() |>
+        ggplot2::ggplot(aes(x = population,
+                            y = violent_crime_rate,)) +
+        ggplot2::geom_point(alpha = 0.5) +
+        geom_smooth(method = "lm") +
+        ggplot2::scale_color_brewer(palette = "Set2") +
+        ggplot2::labs(title = paste('Population vs. Crime Rate in',input$state_plot),
+                      x = 'Population',
+                      y = 'Crime Rate') 
+    )
+  })
+  
+  ## DT TABLE
+  
+  output$table <-  renderDT({
+    
+    # read the documentation for the arguments  
+    datatable(filtered_data_plot()|>
+                select(c('community','type','population','violent_crime_rate')),
+              caption = 'Table: Observations by location.',
+              extensions = 'Scroller',
+              options=list(deferRender = TRUE,
+                           scrollY = 200,
+                           scroller = TRUE))
+    
+  })
+  
+  
+  ## LEAFLET MAP
+  
+  output$map <- leaflet::renderLeaflet({
+    
+    # print(input$daterange)
+    # str(input$daterange)
+    # print(input$table_rows_selected)
+    
+    ## color palette
+    pal <- leaflet::colorNumeric('viridis', 
+                                 domain = data$violent_crime_rate)
+    
+    filtered_data() |> 
+      leaflet::leaflet() |> 
+      leaflet::addProviderTiles(providers$CartoDB.Positron) |> 
+      leaflet::addCircleMarkers(
+        lat = ~latitude,
+        lng = ~longitude,
+        radius = ~violent_crime_rate*20,
+        popup = paste(filtered_data()$community,
+                      "in",
+                      filtered_data()$state_name,
+                      'has crime rate of',
+                      filtered_data()$violent_crime_rate),
+        color = ~pal(violent_crime_rate),
+        options = popupOptions(closeButton = FALSE))
+  })
+}
+
+shinyApp(ui, server)
